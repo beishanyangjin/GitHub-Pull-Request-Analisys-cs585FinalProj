2897ebfdb03408c53522d2c0859c6a7a2bbe6ab4
beishanyangjin <56463501+beishanyangjin@users.noreply.github.com>
2022-12-16 17:45:23 -0500

    Delete my_script.py

--- final/my_script.py
+++
@@ -1,7 +0,0 @@
-from pyspark import SparkConf, SparkContext
-
-conf = SparkConf().setAppName('My App')
-sc = SparkContext(conf=conf)
-
-count = sc.range(1, 1000 * 1000 * 100).filter(lambda x: x > 100).count()
-print('count: ', count)
